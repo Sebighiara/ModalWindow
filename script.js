98a34d7f872fe8937bdafc143922fdf1bf3485d0
Sebighiara <sebi.gg98@gmail.com>
2023-02-08 14:13:59 +0200

    show modal and blur background when clicking a show-modal button

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const btnShowModal = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const btnCloseModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
