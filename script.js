a771b25fd66a9a7eacbe8b2a23a731a6e372bf9a
Sebighiara <sebi.gg98@gmail.com>
2023-02-08 14:26:16 +0200

    close modal when clicking on close button or outside the modal, and applied functions

--- script.js
+++ script.js
@@ -5,9 +5,19 @@ const modal = document.querySelector(".modal");
 const btnCloseModal = document.querySelector(".close-modal");
 const overlay = document.querySelector(".overlay");
 
-for (let i = 0; i < btnShowModal.length; i++) {
-  btnShowModal[i].addEventListener("click", function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
-}
+const showModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const hideModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnShowModal.length; i++)
+  btnShowModal[i].addEventListener("click", showModal);
+
+btnCloseModal.addEventListener("click", hideModal);
+
+overlay.addEventListener("click", hideModal);
